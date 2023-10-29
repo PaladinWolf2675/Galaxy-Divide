053fb9a962277eafe7ad470961c88a20fd802e97
PaladinWolf2675 <timberwolf2675@gmail.com>
2023-10-29 00:05:28 -0400

    New Project created

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set player to default start position
+        // position will be mid screen
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
