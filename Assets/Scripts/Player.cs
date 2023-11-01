6454077dbf1bc50dbad510512e68542f63908cf4
PaladinWolf2675 <timberwolf2675@gmail.com>
2023-10-31 23:05:25 -0400

    added in test script for player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Make player move at normal speed.
+        // Multiply Vector3 By Unity unit by time.deltatime(real time)
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
     }
 }
