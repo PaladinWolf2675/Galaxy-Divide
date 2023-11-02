c55c41e99cb832207cf9d71214934c53fa384dd5
PaladinWolf2675 <timberwolf2675@gmail.com>
2023-11-02 12:16:51 -0400

    Added code to allow for user input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,14 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 5f;
 
+     // Create an input system for player movement
+    // Variable for horizontal movement input
+    // Variable for vertical movement input
+    // Both variables public
+    // Both variables type float
+
+   
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +29,27 @@ public class Player : MonoBehaviour
         // set player starting position = new position (0, 0, 0)
         // always use new Vector3
         transform.position = new Vector3(0, 0, 0);
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+          // Allow for User input on horizontal axis
+       float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Allow for User input on vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        
         // Make player move at normal speed.
-        // Multiply Vector3 By 5 by time.deltatime(real time)
-        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        // Multiply Vector3 By _speed by time.deltatime(real time)
+        // Add in axis variables for user input
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+
+       
     }
 }
