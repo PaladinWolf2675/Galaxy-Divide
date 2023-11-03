4175278e5bea7d26c154cfb58d565d73c1fb6ab4
PaladinWolf2675 <timberwolf2675@gmail.com>
2023-11-03 03:41:32 -0400

    added pseudo code and script for player bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // requirements for variables
-    // public or private reference
-    // data type (int, float, bool, string)
-    // (optional) value assigned
-    // public variables can be modified inside Unity inspector
-    // use underscore for private variables ex private float _speed = 1.0f;
-    // if private variables need to be modified use [SerializeField] above variable in the code
+   
     [SerializeField]
     private float _speed = 5f;
 
-     // Create an input system for player movement
-    // Variable for horizontal movement input
-    // Variable for vertical movement input
-    // Both variables public
-    // Both variables type float
+    
 
    
     
@@ -26,8 +16,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // set player starting position = new position (0, 0, 0)
-        // always use new Vector3
+       
         transform.position = new Vector3(0, 0, 0);
 
         
@@ -45,14 +34,23 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         
         
-        // Make player move at normal speed.
-        // Multiply Vector3 By _speed by time.deltatime(real time)
-        // Add in axis variables for user input
-        // new Vector3(0, 0, 0);  (x, y, z)
-        // Vector3 can be used as variable type- Vector3 direction = new vector3(horizontalInput, verticalInput, 0);
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        // if player position on y is greater than 0
+        // y position = 0
+        // else if player position on y is less than -3.9f
+        // y position = -3.9f
+
+        if (transform.position.y >= 0)
+        {
+             transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.9f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.9f, 0);
+        }
+
 
 
        
