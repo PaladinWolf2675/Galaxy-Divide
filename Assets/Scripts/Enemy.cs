40ea113385cad36d8f18b997101cb72bc36bfffc
PaladinWolf2675 <timberwolf2675@gmail.com>
2023-11-13 19:56:59 -0500

    Add an in vector 3 translation for enemy. Added on trigger enter void method in addition to pseudocode four conditions of method

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _enemySpeed = 4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,25 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         // move enemy down 4 meters per second
+           transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+        //if bottom of screen 
+        //respawn at top with new random x position
+
+        if(transform.position.y <= -5f)
+        {
+            float Randomx = Random.Range(-9f, 9f);
+           transform.position = new Vector3(Randomx, 9f, 0);
+        }
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        // if other is player
+        // damage player 
+        // Destroy Us
+
+        // if other is Laser
+        // Destroy Laser
+        // Destroy Us
     }
 }
