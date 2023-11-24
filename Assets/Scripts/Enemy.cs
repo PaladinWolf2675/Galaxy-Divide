22f4de54243a01be9042304bed90eead9ffaba6b
PaladinWolf2675 <timberwolf2675@gmail.com>
2023-11-24 15:17:21 -0500

    added 2D prefabs and polished gameplay

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -25,7 +25,7 @@ public class Enemy : MonoBehaviour
            transform.position = new Vector3(Randomx, 9f, 0);
         }
     }
-    private void OnTriggerEnter(Collider other) 
+    private void OnTriggerEnter2D(Collider2D other) 
     {
      
           if (other.tag == "Player")
