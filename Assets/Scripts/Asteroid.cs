551626143bd788bf5fa064dd7383dac4fb21df6c
PaladinWolf2675 <timberwolf2675@gmail.com>
2024-01-22 20:23:34 -0500

    Added in asteroid prefab along with new script to control asteroid movement and rotation.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 10f;
+    [SerializeField]
+    private float _asteroidSpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate this object on Z axis
+        transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime, Space.Self);
+        //move asteriod down on Y axis without rotation effecting movement
+        transform.Translate(Vector3.down * _asteroidSpeed * Time.deltaTime, Space.World);
+    }
+}
