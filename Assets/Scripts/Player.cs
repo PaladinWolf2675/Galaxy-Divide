e694b5ee9abc9d757489009065316a715d539e14
PaladinWolf2675 <timberwolf2675@gmail.com>
2025-02-12 04:10:54 -0500

    Added time dot Delta time to player script.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right);
+        // Time.deltaTime = real world time 1 meter per second
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
     }
 }
