b0e50caaf0d85a47e410b083d2410d6fcae9ad66
PaladinWolf2675 <timberwolf2675@gmail.com>
2025-09-02 23:42:33 -0400

    Created triple shot power up script and confirmed proper effect.

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +13,28 @@ public class Powerup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //move down at speed of 3 be sure to serialize
+        //when we leave the screen
+        //destroy us
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if(transform.position.y <= -4.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    //OnTriggerCollision
+    //Only be collectable by the player 
+    //Check for player tag
+    //on collision with player 
+    //destroy us
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy (this.gameObject);
+        }
     }
 }
